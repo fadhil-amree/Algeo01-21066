bc9707e6ddba7e574a8c5e640b9d6f0a5ef415e3
razzanYoni <16521378@mahasiswa.itb.ac.id>
2022-09-28 23:37:07 +0700

    menambah IsFile pada Main

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ package src;
 import java.util.*;
 import src.Matrix;
 public class Main {
-    
-    public static void  main (String[] args){
-        int nRow, nCol, menu; //deklarasi jumlah baris dan kolom pada matriks
-        Scanner input = new Scanner(System.in); //inisiasi variabel untuk melakukan input
-        boolean runProgram = true;
-
-        while (runProgram){
+        
+        public static void  main (String[] args){
+                int nRow, nCol, menu; //deklarasi jumlah baris dan kolom pada matriks
+                Scanner input = new Scanner(System.in); //inisiasi variabel untuk melakukan input
+                boolean runProgram = true;
+                
+                while (runProgram){
             System.out.println("MENU");
             System.out.println("1. Sistem Persamaan Linier");
             System.out.println("2. Determinan");
@@ -18,85 +18,108 @@ public class Main {
             System.out.println("6. Regresi Linier Berganda");
             System.out.println("7. Keluar");
             System.out.println("");
-
+            
             System.out.print("Masukkan Pilihan Anda: ");
             menu = input.nextInt();
-
+            
             //Template
             switch (menu){
-                case 1:
-                        System.out.println("1. Metode Eliminasi Gauss");
-                        System.out.println("2. Metode Eliminasi Gauss-Jordan");
-                        System.out.println("3. Metode Matriks Balikan");
-                        System.out.println("4. Kaidah Cramer");
-                        System.out.println("");
-
-                        System.out.print("Masukkan Pilihan Anda: ");
-                        menu = input.nextInt();
+                    case 1:
+                    System.out.println("1. Metode Eliminasi Gauss");
+                    System.out.println("2. Metode Eliminasi Gauss-Jordan");
+                    System.out.println("3. Metode Matriks Balikan");
+                    System.out.println("4. Kaidah Cramer");
+                    System.out.println("");
+                    
+                    System.out.print("Masukkan Pilihan Anda: ");
+                    menu = input.nextInt();
                         
-                        switch (menu){
+                    switch (menu){
                             case 1: System.out.println("Menyelesaikan SPL dengan Metode Eliminasi Gauss");
-                                    break;
+                            break;
                             case 2: System.out.println("Menyelesaikan SPL dengan Metode Eliminasi Gauss-Jordan");
-                                    break;
+                            break;
                             case 3: System.out.println("Menyelesaikan SPL dengan Metode Matriks Balikan");
-                                    break;
+                            break;
                             case 4: System.out.println("Menyelesaikan SPL dengan Kaidah Cramer");
-                                    break;
+                            break;
                             default: System.out.println("Pilihan Anda tidak terdapat pada menu!");
-                                    break;
+                            break;
                         }
                         break;
-                case 2:
+                        case 2:
+                        // handle dengan IsSquare
                         System.out.println("1. Metode Reduksi Baris");
                         System.out.println("2. Metode Ekspansi Kofaktor"); 
                         System.out.println("");
-
+                        
                         System.out.print("Masukkan Pilihan Anda: ");
                         menu = input.nextInt();     
                         
                         switch(menu){
-                            case 1: System.out.println("Mencari Determinan Matriks dengan Metode Reduksi Baris");
-                                    break;
-                            case 2: System.out.println("Mencari Determinan Matriks dengan Metode Ekspansi Kofaktor");
-                                    break;
-                            default: System.out.println("Pilihan Anda tidak terdapat pada menu!");
-                                    break;
+                                case 1: System.out.println("Mencari Determinan Matriks dengan Metode Reduksi Baris");
+                                break;
+                                case 2: System.out.println("Mencari Determinan Matriks dengan Metode Ekspansi Kofaktor");
+                                break;
+                                default: System.out.println("Pilihan Anda tidak terdapat pada menu!");
+                                break;
                         }
                         break;
-                case 3: 
+                        case 3: 
+                        // handle dengan isInvertible
                         System.out.println("1. Metode Reduksi Baris");
                         System.out.println("2. Metode Adjoin"); 
                         System.out.println("");
-
+                        
                         System.out.print("Masukkan Pilihan Anda: ");
                         menu = input.nextInt();      
-                    
+                        
                         switch(menu){
-                            case 1: System.out.println("Mencari Matriks Balikan dengan Metode Reduksi Baris");
-                                    break;
-                            case 2: System.out.println("Mencari Matriks Balikan dengan Metode Ekspansi Kofaktor");
-                                    break;
-                            default: System.out.println("Pilihan Anda tidak terdapat pada menu!");
-                                    break;
+                                case 1: System.out.println("Mencari Matriks Balikan dengan Metode Reduksi Baris");
+                                break;
+                                case 2: System.out.println("Mencari Matriks Balikan dengan Metode Ekspansi Kofaktor");
+                                break;
+                                default: System.out.println("Pilihan Anda tidak terdapat pada menu!");
+                                break;
                         }
                         break;     
-                case 4: 
+                        case 4: 
                         System.out.println("Melakukan Interpolasi Polinom");
                         break;
-                case 5:
+                        case 5:
                         System.out.println("Melakukan Interpolasi Bicubic");
                         break;
-                case 6: 
+                        case 6: 
                         System.out.println("Melakukan Regresi Linier Berganda");
                         break;
-                case 7:
+                        case 7:
                         System.out.println("Keluar dari Program");
                         runProgram = false;
                         break;
-                default: System.out.println("Pilihan Anda tidak terdapat pada menu!");
-                         break;
-            }
+                        default: System.out.println("Pilihan Anda tidak terdapat pada menu!");
+                        break;
+                }
         }
-    }
+}
+        public Matrix IsFile()
+        {
+                int IsInput;
+                Matrix matrix = new Matrix(); 
+                Scanner input = new Scanner(System.in);
+                System.out.println("1. Input dari keyboard");
+                System.out.println("2. Input dari file");
+                System.out.println("");
+
+                System.out.print("Masukkan Pilihan Anda: ");
+                IsInput = input.nextInt();
+
+                if (IsInput == 1)
+                {
+                        return matrix ;
+                        // Input dari keyboard
+                } else // IsFile == 2
+                {
+                        // Input dari file
+                }
+        }
 }
