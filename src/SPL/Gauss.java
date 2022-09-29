bae25e9cf0fbc0dfb2a6076474544473c2dfebd7
razzanYoni <16521378@mahasiswa.itb.ac.id>
2022-09-29 08:37:21 +0700

    benerin implementasi gaussnya

--- src/SPL/Gauss.java
+++ src/SPL/Gauss.java
@@ -7,9 +7,10 @@ public class Gauss {
     public void splbyGauss(Matrix matrix)
     {
         // KAMUS
-        Matrix matriks = MatReduksi(matrix);
+        /* Lakukan OBE */
         int i1, j1, j2, k, n; /*iterasi*/
         float temp;
+        matrix = MatOBE(matrix);  // OBE
 
         // ALGORITMA
         for (i1 = 0; i1 < matrix.getNRow(); i1++)
@@ -35,4 +36,68 @@ public class Gauss {
         /* Kasus 3 : Tidak ada Solusi */
 
     }
+
+    public Matrix MatOBE(Matrix matrix)
+    {
+        float temp;
+        
+        // KAMUS
+
+        // ALGORITMA
+        while (!(ReduksiBaris.IsSegitiga(matrix)))
+        {
+            // Tukar baris matrix yang elemen diagonalnya 0
+            int i, j;
+            for (i = 0; i < matrix.getNCol(); i++)
+            {
+                if (matrix.getElmtContent(i, i) == 0)
+                {
+                    j = i + 1;
+                    while (j < matrix.getNCol())
+                    {
+                        if (matrix.getElmtContent(j, i)!= 0)
+                        {
+                            ReduksiBaris.tukarBaris(matrix, i, j);
+                        }
+                        j++;
+                    }
+                }
+            }
+
+            // tambah baris dengan baris lainnya
+            for (i = 0; i < matrix.getNCol(); i++)
+            {
+                for (j = 0; j < i; j++)
+                {
+                    // Cek apakah elemen pada matrix bernilai 0
+                    if (matrix.getElmtContent(i, j) != 0)
+                    {
+
+                        // Cari KPK
+                        float KPK = matrix.getElmtContent(i, j) * matrix.getElmtContent(j, j);
+                        float kali = KPK / matrix.getElmtContent(i, j);
+
+                        // kalikan baris i dengan konstanta kali
+                        int col;
+                        for (col = 0; col < matrix.getNCol(); col++)
+                        {
+                            temp = matrix.getElmtContent(i, col) * kali;
+                            matrix.setElmtContent(i, col, temp);
+                        }
+
+                        float kelipatan = KPK / matrix.getElmtContent(j, j);
+
+                        for (col = 0; col < matrix.getNCol(); col++)
+                        {
+                            temp = matrix.getElmtContent(i, col) - (matrix.getElmtContent(j, col) * kelipatan);
+                            matrix.setElmtContent(i, col, temp);
+                        }
+                    }
+                }
+            }
+        }
+        return matrix;
+    }
+
+    
 }
